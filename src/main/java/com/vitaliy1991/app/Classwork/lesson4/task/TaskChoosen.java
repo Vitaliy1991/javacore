b8b4add6f02c69f4e221120234ae29d2f9201564
Vitaliy Torkotiuk <vitor@ciklum.com>
2017-02-19 18:37:17 +0200

    Lesson 4

---
+++ src/main/java/com/vitaliy1991/app/Classwork/lesson4/task/TaskChoosen.java
@@ -0,0 +1,31 @@
+package com.vitaliy1991.app.Classwork.lesson4.task;
+
+import com.vitaliy1991.app.Classwork.lesson4.biggerNumber.BiggerNumber;
+import com.vitaliy1991.app.Classwork.lesson4.circleArea.CircleArea;
+import com.vitaliy1991.app.Classwork.lesson4.triangle.myCheck.TriangleAngel;
+import com.vitaliy1991.app.Classwork.lesson4.triangle.triangleClasswork.RightAngledRunner;
+import com.vitaliy1991.app.Classwork.lesson4.triangle.triangleClasswork.RightAngledTriangle;
+
+import java.util.Scanner;
+
+/**
+ * Created by vitor on 19-Feb-17.
+ */
+public class TaskChoosen {
+    public static void chooseTask() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter task number: ");
+        double task = scanner.nextDouble();
+        if (task == 1) {
+            TriangleAngel.triangle();
+        } else {
+            if (task == 2) {
+                CircleArea.calculateBigger();
+            } else {
+                if (task == 3) {
+                    BiggerNumber.calculateNumber();
+                } else {System.out.print("There is no such task number. Try to choose 1, 2, 3");}
+            }
+        }
+    }
+}
---
+++ src/test/java/com/vitaliy1991/app/homework/lesson4/ClassworkTest.java
@@ -0,0 +1,19 @@
+package com.vitaliy1991.app.homework.lesson4;
+
+import com.vitaliy1991.app.Classwork.lesson4.biggerNumber.BiggerNumber;
+import com.vitaliy1991.app.Classwork.lesson4.triangle.triangleClasswork.RightAngledRunner;
+import com.vitaliy1991.app.Classwork.lesson4.triangle.triangleClasswork.RightAngledTriangle;
+import com.vitaliy1991.app.homework.lesson3_unitTest.ArithmeticFunc;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by vitor on 19-Feb-17.
+ */
+/*public class ClassworkTest {
+        @Test
+        public void rightAngeledTests(){
+            int a = 4, b = 5, c = 6, expRes = 10;
+            Assert.assertEquals(expRes, RightAngledTriangle.);
+        }
+}*/
